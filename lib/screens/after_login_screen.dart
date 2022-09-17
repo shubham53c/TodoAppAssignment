9ab596a9b06fd0b86bf7155004859a671525f506
Shubham Chourasia <shubhamchourasia53@gmail.com>
2022-09-18 02:11:54 +0530

    Firebase CRUD

--- lib/screens/after_login_screen.dart
+++ lib/screens/after_login_screen.dart
@@ -1,11 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:todoapp/screens/login_screen.dart';
+import './login_screen.dart';
 
 class AfterLogin extends StatelessWidget {
   AfterLogin({Key? key}) : super(key: key);
 
   final user = FirebaseAuth.instance.currentUser!;
+
+  CollectionReference<Map<String, dynamic>> get _collection =>
+      FirebaseFirestore.instance.collection(user.email!);
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +41,67 @@ class AfterLogin extends StatelessWidget {
                 }
               },
               child: const Text("Sign Out"),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                try {
+                  _collection.get().then((querySnapshot) {
+                    print("leng: ${querySnapshot.docs.length}");
+                    for (var doc in querySnapshot.docs) {
+                      print("Doc id: ${doc.id}");
+                      print("dfdf: ${doc["taskDesc"]}");
+                    }
+                  });
+                } catch (e) {
+                  print("error: $e");
+                }
+              },
+              child: const Text("Fetch Data"),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                try {
+                  _collection.add({
+                    "taskName": "Test",
+                    "taskDesc": "This is a test",
+                    "taskTime": DateTime.now().toIso8601String(),
+                  }).then((querySnapshot) {});
+                } catch (e) {
+                  print("error: $e");
+                }
+              },
+              child: const Text("Add Data"),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                try {
+                  _collection.doc("L9EZUvETC4wdyhk9dUdW").update({
+                    "taskName": "Test1234",
+                    "taskDesc": "This is a test1233",
+                    "taskTime": DateTime.now().toIso8601String(),
+                  }).then((querySnapshot) {});
+                } catch (e) {
+                  print("error: $e");
+                }
+              },
+              child: const Text("Update Data"),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                try {
+                  _collection
+                      .doc("gHoKYwAZtJkCSJUb7D8b")
+                      .delete()
+                      .then((querySnapshot) {});
+                } catch (e) {
+                  print("error: $e");
+                }
+              },
+              child: const Text("Delete Data"),
             ),
           ],
         ),
