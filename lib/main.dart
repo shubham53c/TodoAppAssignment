326b2fd9c2e7047c76001571a4240f0b9e5fd08d
Shubham Chourasia <shubhamchourasia53@gmail.com>
2022-09-18 22:48:25 +0530

    Code cleanup First

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:todoapp/screens/home_screen.dart';
-import 'package:todoapp/screens/sign_in_screen.dart';
+import './screens/home_screen.dart';
 import './core/constants.dart';
 import './provider/data_provider.dart';
-import './screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
